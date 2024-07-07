3654c4cde491345e57915d9a4d777e72f5471d25
YunFy26 <207946431@qq.com>
2024-07-07 16:32:41 +0800

    optimize prompt

--- prompt.py
+++ prompt.py
@@ -8,40 +8,86 @@ class Prompt:
     You are a seasoned penetration testing expert with extensive experience in the field. You hold multiple prestigious certifications, including OSCP (Offensive Security Certified Professional), CEH (Certified Ethical Hacker), and CISSP (Certified Information Systems Security Professional). Your expertise spans across various domains, including network security, application security, and vulnerability assessment. You are well-versed in using a wide array of penetration testing tools and methodologies to identify and exploit vulnerabilities, and you follow the latest industry best practices and standards.
     """
     split_task_prompt: str = """你是一名经验丰富的渗透测试专家，在该领域拥有广泛的经验。你持有多项知名认证，包括 OSCP（Offensive Security Certified Professional）、CEH（Certified Ethical Hacker）和 CISSP（Certified Information Systems Security Professional）。你的专业领域包括网络安全、应用程序安全和漏洞评估。你熟练使用各种渗透测试工具和方法来识别和利用漏洞，并遵循最新的行业最佳实践和标准。
-    用户给出的任务是：{task}，你需要做的是: 
-    1. 将用户给出的任务分解为一个或多个子任务，然后维护一个“任务列表”，你应该将任务显示在层级结构中，例如1, 1.1, 1.1.1, 1.2, 1.2.1...；
-    2. 确保每个子任务可以由以下给出的工具完成：{tools_description}
+    你的任务是根据给定的输入提供详细的逐步说明。
+    1. 将用户给出的任务分解为一个或多个子任务，然后维护一个“任务列表”，你应该将任务显示在层级结构中；
+    2. 请根据任务类型划分子任务，比如，扫描不同的端口属于同一类型的任务，不要根据端口数量划分子任务，请牢记这一点；
     3. 你无需判断应该用什么工具去完成子任务，但你应该确保每个子任务最多调用一个工具即可获得结果；
-    4. 输出这个任务列表
+    4. 如果是端口扫描任务，请不要根据端口数量划分子任务；
     
-    
-    最终返回的response应该遵循下面的格式:{response_format}
-    
+    给定的输入是:{task};
+    你需要遵循如下约束条件{constraints},
+    你最终返回的response应该遵循下面的格式:{split_task_response_format}
     """
 
-    constraints_1: str = """1.每个子任务最多调用一个工具即可获得结果，如果一个子任务需要多个工具才能完成，
+    split_task_response_format: str = """
+    response:
+            {
+                "task": "用户给出的任务",
+                "subtask_list": 
+                [
+                    {
+                        "task_id": "1",
+                        "description": "子任务1描述"
+                    },
+                    {
+                        "task_id": "2",
+                        "description": "子任务2描述"
+                    },
+                    {
+                        "task_id": "3",
+                        "description": "子任务3描述"
+                    }
+                ]
+            }
+    """
+
+    constraint_split_1: str = """1.请注意，每个子任务最多调用一个工具即可获得结果，如果一个子任务需要多个工具才能完成，
     那么你需要将这个子任务进一步细分为多个子任务，直到每个子任务最多调用一个工具。
     如果用户给出的任务已经是最细粒度的子任务，那么无需再对任务进行细分。"""
-    constraints_2: str = """2.请注意，你的response会被用作另一个大模型的input，
+    constraint_split_2: str = """2.请注意，你的response会被用作另一个大模型的input，
     由于token大小限制，你在描述子任务时，应尽量保证描述清晰、简短，不要出现不必要的描述"""
+    constraint_split_3: str = """3.请注意，子任务列表中不应该出现类似“选择xxxx工具”这样的话，因为你只需要分解任务，不需要选择工具"""
+    # constraint_4: str = """4.请注意，请根据任务类型划分子任务，比如，扫描不同的端口属于同一类型的任务，不需要根据端口数量划分子任务，请牢记这一点"""
 
+    action_task_prompt: str = """你是一名经验丰富的渗透测试专家，在该领域拥有广泛的经验。你持有多项知名认证，包括 OSCP（Offensive Security Certified Professional）、CEH（Certified Ethical Hacker）和 CISSP（Certified Information Systems Security Professional）。你的专业领域包括网络安全、应用程序安全和漏洞评估。你熟练使用各种渗透测试工具和方法来识别和利用漏洞，并遵循最新的行业最佳实践和标准。
+    你的任务是：
+    1. 根据给定的任务列表，选择合适的工具执行任务，工具列表见稍后的描述;
+    2. 你需要给出详细的逐步说明，包括你的思考过程、选择的工具、工具的输入、工具的执行结果、对工具执行结果的分析;
+    3. 如果你在执行渗透测试的过程中发现了漏洞（CVE或CWE），请你给出漏洞的详细描述，影响范围，以及修补方案;
+    4. 你需要按照顺序执行任务列表中的任务，如果上次执行了子任务1，那么这次就要执行子任务2，同时更新子任务1中的工具执行结果;
+    
+    任务列表是:{task_list};
+    你能够调用的工具列表是:{tools_description}，你只能使用这些工具来执行任务;
+    你需要遵循如下约束条件:{constraints};
+    你上一次执行的任务id是:{task_id}，这次应该执行任务{cur_task_id};
+    你上一次使用的工具的名称是:{action_name},参数是:{action_args},工具的输出结果是:{action_result};
+    你最终返回的response应该遵循下面的格式:{action_task_response_format}
+    """
 
-    response_format = """
+    constraint_action_1: str = """1. 请注意，任务列表中包含了用户的原始输入任务task和已经分解后的子任务列表subtask_list，你只需要执行subtask_list中的任务"""
+    constraint_action_2: str = """2. 请注意，你需要按照顺序执行任务，比如这次执行了id为2的任务，那么下次就是执行id为3的任务，以此类推"""
+    constraint_action_3: str = """3. 请注意，你的response会被用作另一个大模型的input，由于token大小限制，你在描述子任务时，应尽量保证描述清晰、简短，不要出现不必要的描述"""
+    constraint_action_4: str = """4. 请注意，由于工具的执行结果在下一次prompt中反馈给你，所以你需要在接收到下一次prompt中工具的执行结果后，对上一次返回的response进行修正"""
+
+    action_task_response_format = """
     response:
             {
                 "action": {
-                    "name": "action_name",
-                    "args": {
-                        "args name": "args value"
-                    }
-                },
-                "thoughts":{
-                    "reasoning": "the reasoning about the question",
-                    "plan": "the plan you have to solve the question",
-                    "action": "the action you have to take",
-                    "action input": "the input of the action",
-                    "observation": "the result of the action"
-                    "revision": "according to the result, revise the response"
+                    "action_subtask_id": "本次执行的任务的id",
+                    "reasoning": "你的思考过程",
+                    "tool_name": "执行本次任务调用的工具名",
+                    "tool_args": 
+                    {
+                        "args_name": "args_value" ，执行本次任务调用的工具的参数
+                    },
+                    "tool_output": "工具的执行结果,这个执行结果会在下一次的prompt中提到",
+                    "result_analysis": "在接收到下一次prompt中工具的执行结果后，对工具执行结果的分析",
+                    "found_cve": {
+                        "cve_id": "CVE-xxxx-xxxx",
+                        "description": "漏洞的详细描述",
+                        "impact": "影响范围",
+                        "fix": "修补方案"
+                    },如果你在执行渗透测试的过程中发现了漏洞，请给出漏洞的cve_id, 详细描述，影响范围，以及修补方案，如果没有发现，则found_cve中各字段值为null"
                 }
             }
     """
@@ -49,13 +95,31 @@ class Prompt:
     tools_description: str = gen_tools_desc()
 
 
-def gen_prompt(task, revision):
+def gen_split_task_prompt(task) -> str:
     prompt = Prompt()
-    return prompt.prompt_template.format(
+    return prompt.split_task_prompt.format(
         task=task,
+        # tools_description=prompt.tools_description,
+        constraints=prompt.constraint_split_1 + '\n' + prompt.constraint_split_2 + '\n' + prompt.constraint_split_3,
+        split_task_response_format=prompt.split_task_response_format)
+
+
+def gen_action_task_prompt(task_list, task_id, action_name, action_args, action_result) -> str:
+    prompt = Prompt()
+    return prompt.action_task_prompt.format(
+        task_list=task_list,
         tools_description=prompt.tools_description,
-        revision=revision,
-        response_format=prompt.response_format)
+        constraints=prompt.constraint_action_1 + '\n' + prompt.constraint_action_2 + '\n'
+                    + prompt.constraint_action_3 + '\n' + prompt.constraint_action_4,
+        task_id=task_id,
+        cur_task_id=str(int(task_id) + 1),
+        action_name=action_name,
+        action_args=action_args,
+        action_result=action_result,
+        action_task_response_format=prompt.action_task_response_format
+    )
 
 
-user_prompt = "根据给定的目标和迄今为止取得的进展，确定下一步action，并维护任务列表,把最终的任务列表写入文件，使用前面指定的JSON模式进行响应："
+user_split_task_prompt = "请你按照要求进行任务分解，按照指定的response格式进行响应."
+
+user_action_task_prompt = "根据本次需要执行的任务id和所给出的工具，选择合适的工具执行任务，并遵从约束条件，按照指定的response格式进行响应."
